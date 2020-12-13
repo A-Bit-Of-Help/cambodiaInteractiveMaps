74e2bb6aae4160f463f0fa1ec7e9da4bbfec5254
LookasNorman <lookasziebice@gmail.com>
2020-12-13 12:26:18 +0100

    Created interactive maps

---
+++ js/cambodiaMap.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $("#SreiSnam").click(function(){
+        $("#mSreiSnam").modal();
+    });
+    $("#Varin").click(function(){
+        $("#mVarin").modal();
+    });
+    $("#SiemReap").click(function(){
+        $("#mSiemRap").modal();
+    });
+    $("#KompongLeaeng").click(function(){
+        $("#mKompongLeaeng").modal();
+    });
+    $("#Kralanh").click(function(){
+        $("#mKralanh").modal();
+    });
+    $("#TaingKork").click(function(){
+        $("#mTaingKork").modal();
+    });
+});
