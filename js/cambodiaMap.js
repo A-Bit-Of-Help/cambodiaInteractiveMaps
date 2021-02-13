1ad969e7e2c34aee48be1644e4c9b8106917f81f
Rafał Michniuk <38009080+RafalMichniuk@users.noreply.github.com>
2021-02-13 13:13:21 +0100

    added hover for names and pins

--- js/cambodiaMap.js
+++ js/cambodiaMap.js
@@ -30,4 +30,30 @@ $(document).ready(function () {
     $('#KH-6').click(function () {
         $('#mTaingKork').modal();
     });
+
+    $('[data-hover="KompongLeaeng"]')
+        .mouseenter(function () {
+            $('#KH-4').css({ stroke: '#f8f9fa', fill: '#000' });
+        })
+        .mouseleave(function () {
+            $('#KH-4').css({ stroke: 'none', fill: '#a5ce3b' });
+        });
+
+    $('[data-hover="TaingKork"]')
+        .mouseenter(function () {
+            $('#KH-6').css({ stroke: '#f8f9fa', fill: '#000' });
+        })
+        .mouseleave(function () {
+            $('#KH-6').css({ stroke: 'none', fill: '#a5ce3b' });
+        });
+
+    $('[data-hover="SiemReap"]')
+        .mouseenter(function () {
+            $('#KH-17').css({ stroke: '#f8f9fa', fill: '#000' });
+            $('[data-name="province"]').css('stroke', '#f8f9fa');
+        })
+        .mouseleave(function () {
+            $('#KH-17').css({ stroke: 'none', fill: '#a5ce3b' });
+            $('[data-name="province"]').css('stroke', 'none');
+        });
 });
