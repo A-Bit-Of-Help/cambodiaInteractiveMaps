8836109790a4c887e077df5b9d797ce0d3bcced2
Rafał Michniuk <38009080+RafalMichniuk@users.noreply.github.com>
2021-03-04 21:22:14 +0100

    add city modal automatization bind to city name

--- js/cambodiaMap.js
+++ js/cambodiaMap.js
@@ -1,25 +1,5 @@
-$(document).ready(function () {
-    $('#SreiSnam').click(function () {
-        $('#mSreiSnam').modal();
-    });
-
-    $('#Varin').click(function () {
-        $('#mVarin').modal();
-    });
-
-    $('#SiemReap').click(function () {
-        $('#mSiemRap').modal();
-    });
-
-    $('#KompongLeaeng').click(function () {
-        $('#mKompongLeaeng').modal();
-    });
-
-    $('#Kralanh').click(function () {
-        $('#mKralanh').modal();
-    });
-
-    $('#TaingKork').click(function () {
-        $('#mTaingKork').modal();
+$(document).ready(() => {
+    $("g[data-name='city']").each((index, item) => {
+        $(`#${item.id}`).click(() => $(`#m${item.id}`).modal());
     });
 });
