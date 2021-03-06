763bc01e0ce9a4695927c5baa83e98640c209d08
Rafał Michniuk <38009080+RafalMichniuk@users.noreply.github.com>
2021-03-06 11:55:28 +0100

    add short note for future city HTML tags extends

--- js/cambodiaMap.js
+++ js/cambodiaMap.js
@@ -1,3 +1,5 @@
+// IMPORTANT !!!
+// NOTE: every new g tag with city in HTML has to contain data-tag: data-name='city'
 $(document).ready(() => {
     $("g[data-name='city']").each((index, item) => {
         $(`#${item.id}`).click(() => $(`#m${item.id}`).modal());
